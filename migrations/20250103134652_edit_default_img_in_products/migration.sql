cddb792bcd106ff37b604a085d2b3c10ec61e67f
okmic <okmic.g@gmail.com>
2025-01-03 16:47:31 +0300

    edit_default_img_in_products

---
+++ migrations/20250103134652_edit_default_img_in_products/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE `Content` MODIFY `mainBlockTitle` VARCHAR(191) NOT NULL DEFAULT 'Mico Page 👋';
+
+-- AlterTable
+ALTER TABLE `SiteProducts` MODIFY `imgUrl` VARCHAR(191) NOT NULL DEFAULT '/media/icons/default.png';
